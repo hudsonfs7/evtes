c1ef24f808f285ae5c09f20101dbaa6c83997ea5
hudsonfs7 <hudsonfs7@hotmail.com>
2025-07-21 14:23:00 -0300

    Consertado alinhamento da modal

---
+++ bd/db.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyAIHl7KE9FuYhCF8PG_rw3WQs60aBr1-cU',
+  authDomain: 'evtes-fd2ae.firebaseapp.com',
+  projectId: 'evtes-fd2ae',
+  storageBucket: 'evtes-fd2ae.firebasestorage.app',
+  messagingSenderId: '64658752945',
+  appId: '1:64658752945:web:c63565da04c58b6f2f2990'
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+const db = getFirestore(app)
+
+export { app, db }
